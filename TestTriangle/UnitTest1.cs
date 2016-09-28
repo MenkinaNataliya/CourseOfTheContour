d2eba1e54654bbfe97fa740a2a0205629555e5d4
MenkinaNataliya <tasia-@mail.ru>
2016-09-29 00:53:48 +0500

    First result

---
+++ TestTriangle/UnitTest1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle1;
+
+namespace TestTriangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        public void TestHerons(Triangle tr, double ExectedResult)
+        {
+            var result = tr.Heron();
+
+            Assert.AreEqual(ExectedResult, result, 10e-2);
+        }
+
+        public void TestTriangle(Triangle tr, double ExectedAngleA, double ExectedAngleB=0, double ExectedAngleC=0, double ExectedA =0, double ExectedB= 0, double ExectedC= 0)
+        {
+
+            Assert.AreEqual(ExectedAngleA, tr.angleA, 10e-2);
+            Assert.AreEqual(ExectedAngleB, tr.angleB, 10e-2);
+            Assert.AreEqual(ExectedAngleC, tr.angleC, 10e-2);
+            Assert.AreEqual(ExectedA, tr.a, 10e-2);
+            Assert.AreEqual(ExectedB, tr.b, 10e-2);
+            Assert.AreEqual(ExectedC, tr.c, 10e-2);
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Triangle tr = new Triangle(a: 14, b: 25, angleC:30);
+            TestHerons(tr, 87.50);
+           
+            Assert.AreEqual(28.53, tr.angleA, 10e-2);
+            Assert.AreEqual(121.47, tr.angleB, 10e-2);
+            Assert.AreEqual(14.66, tr.c, 10e-2);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Triangle tr = new Triangle(a: 3, b: 5, angleC: 15);
+            TestHerons(tr, 1.94);
+            
+            Assert.AreEqual(20.27, tr.angleA, 10e-2);
+            Assert.AreEqual(144.73, tr.angleB, 10e-2);
+            Assert.AreEqual(2.24, tr.c, 10e-2);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Triangle tr = new Triangle(a: 12, angleB: 25, angleC: 15);
+            TestHerons(tr,12.25);
+            
+            Assert.AreEqual(140, tr.angleA, 10e-2);
+            Assert.AreEqual(4.83, tr.c, 10e-2);
+            Assert.AreEqual(7.89, tr.b, 10e-2);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Triangle tr = new Triangle(a: 3, b: 5, c: 6);
+            TestHerons(tr, 7.48);
+
+            Assert.AreEqual(29.93, tr.angleA, 10e-2);
+            Assert.AreEqual(56.25, tr.angleB, 10e-2);
+            Assert.AreEqual(93.82, tr.angleC, 10e-2);
+
+        }
+
+
+        [TestMethod]
+        public void TriangleDoesNot()
+        {
+            Triangle tr = new Triangle(a: 3, b: 5, c: 15);
+
+            TestHerons(tr, 0);
+            TestTriangle(tr, 0,0,0,0,0,0);
+
+        }
+
+        [TestMethod]
+        public void NegativeSideA()
+        {
+            Triangle tr = new Triangle(a: 0, b: 5, c: 15);
+
+            TestHerons(tr, 0);
+            TestTriangle(tr, 0, 0, 0, 0, 0, 0);
+        }
+
+
+        [TestMethod]
+        public void NegativeSide()
+        {
+            Triangle tr = new Triangle(a: -3, b: 5, c: 15);
+
+            TestHerons(tr, 0);
+            TestTriangle(tr, 0, 0, 0, 0, 0, 0);
+        }
+
+        [TestMethod]
+        public void NegativeAngle()
+        {
+            Triangle tr = new Triangle(a: -3, b: 5, angleC: -15);
+
+            TestHerons(tr, 0);
+            TestTriangle(tr, 0, 0, 0, 0, 0, 0);
+        }
+
+
+        [TestMethod]
+        public void FunctionalTestTriangleDoesNot()
+        {
+            var rnd = new Random(1);
+            for (int i = 0; i < 100; i++)
+            {
+                var a = rnd.NextDouble() * 10-20;
+                var b = rnd.NextDouble() * 10-20;
+                var c = rnd.NextDouble() * 10-20;
+
+                Triangle tr = new Triangle(a: a, b: b, c: c);
+                if(!(Math.Abs(a) > Math.Abs(b - c) && a < b + c &&
+                                Math.Abs(b) > Math.Abs(a - c) && b < a + c &&
+                                Math.Abs(c) > Math.Abs(b - a) && c < b + a))
+                {
+                    TestHerons(tr, 0);
+                    TestTriangle(tr, 0, 0, 0, 0, 0, 0);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Triangle1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle1
+{
+    public class Triangle
+    {
+
+        public double a { get; set; }
+        public double b { get; set; }
+        public double c { get; set; }
+        public double angleA { get; set; }
+        public double angleB { get; set; }
+        public double angleC { get; set; }
+
+        public Triangle(double a, double b=0, double c=0, double angleA=0, double angleB=0, double angleC=0) {
+
+            if (a <= 0 || b < 0 || c < 0 || angleA < 0 || angleB < 0 || angleC < 0  ) goto DoesNot;
+            else {
+                this.a = a;
+                if (b == 0 && c == 0 && angleA == 0)
+                {//признак по 2 углам и стороне между ними
+                    this.angleB = angleB;
+                    this.angleC = angleC;
+                    this.angleA = 180 - this.angleB - this.angleC;
+                    this.b = SineTheorem(this.a, this.angleB, this.angleA);
+                    this.c = SineTheorem(this.a, this.angleC, this.angleA);
+                }
+                else
+                {
+                    this.b = b;
+                    if (c == 0 && angleA == 0 && angleB == 0)//признак по 2 сторонам и углу
+                    {
+                        this.angleC = angleC;
+                        this.c = Math.Sqrt(b * b + a * a - 2 * b * a * Math.Cos(angleC * Math.PI / 180));
+                        this.angleA = CosineTheorem(this.b, this.c, this.a) ;
+                        this.angleB = 180 - this.angleA - this.angleC;
+                    }
+                    else
+                       if (angleA == 0 && angleB == 0 && angleC == 0)//признак по 3 сторонам
+                        {
+                            if (Math.Abs(a) > Math.Abs(b - c) && a < b + c &&
+                                Math.Abs(b) > Math.Abs(a - c) && b < a + c &&
+                                Math.Abs(c) > Math.Abs(b - a) && c < b + a)
+                            {
+                                this.c = c;
+                                this.angleA = CosineTheorem(this.b, this.c, this.a);
+                                this.angleB = CosineTheorem(this.a, this.c, this.b);
+                                this.angleC = 180 - this.angleB - this.angleA;
+                            }
+                            else goto DoesNot;
+
+                        }
+                }
+                return;      
+            }
+
+            DoesNot:
+            this.a = 0;
+            this.b = 0;
+            this.c = 0;
+            this.angleA = 0;
+            this.angleB = 0;
+            this.angleB = 0;
+        }
+
+        static void Main(string[] args)
+        {}
+
+        public double Heron()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public double CosineTheorem(double a, double b, double c) {
+            return Math.Acos((a * a + b * b - c *c) / (2 * a * b)) * 180 / Math.PI ;
+        }
+
+        public double SineTheorem(double a, double angleA, double angleC)
+        {
+            return a * (Math.Sin(angleA * Math.PI / 180) / Math.Sin(angleC * Math.PI / 180));
+        }
+    }
+}
