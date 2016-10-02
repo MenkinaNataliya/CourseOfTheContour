121a127bac84a97528621dc03a77869e91e390e9
MenkinaNataliya <tasia-@mail.ru>
2016-10-02 15:05:44 +0500

    Easy implementation

---
+++ barley-break/TestBarleyBreak/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using barley_break;
+
+namespace TestBarleyBreak
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void GoingBeyondBoundsOfarray()
+        {
+            int[] tag = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+            Game field = new Game(tag);
+            field.GetLocation(field[4, 1]);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void IncorrectNumberOfArguments()
+        {
+            int[] tag = { 1, 2, 3, 4, 5, 6, 7, 8};
+            Game field = new Game(tag);
+            
+        }
+
+        [TestMethod]
+        public void SuccessfulDeterminingElementCoordinates() {
+            int[] tag = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+            Game field = new Game(tag);
+
+            int[] res = field.GetLocation(5);
+
+            Assert.AreEqual(5, field[ res[0], res[1]]);
+
+        }
+
+
+        [TestMethod]
+        public void DisastrousDeterminingElementCoordinates() {
+            int[] tag = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+            Game field = new Game(tag);
+            int[] res = field.GetLocation(9);
+            Assert.AreEqual(null, res);
+        }
+
+        [TestMethod]
+        public void SuccessfulMovementOfChips()
+        {
+            int[] tag = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+            Game field = new Game(tag);
+
+            int[] coordinate;
+            int[] coordinateZero ;
+            int x;
+            int y;
+            int xZero;
+            int yZero;
+
+            for (int i = 1; i < tag.Length; i++)
+            {
+                coordinate = field.GetLocation(i);
+                x = coordinate[0];
+                y = coordinate[1];
+
+                coordinateZero = field.GetLocation(0);
+                xZero = coordinateZero[0];
+                yZero = coordinateZero[1];
+
+                if (Math.Abs(x - xZero) == 1 && Math.Abs(y - yZero) == 0
+                    || Math.Abs(x - xZero) == 0 && Math.Abs(y - yZero) == 1)
+                {
+                    field.Shift(i);
+                    Assert.AreEqual(0, field[x, y]);
+                    Assert.AreEqual(i, field[xZero, yZero]);
+                }
+            }
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void DisastrousMovementOfChips()
+        {
+            int[] tag = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+            Game field = new Game(tag);
+
+            int[] coordinate ;
+            int[] coordinateZero = field.GetLocation(0);
+            int x ;
+            int y ;
+            int xZero = coordinateZero[0];
+            int yZero = coordinateZero[1];
+
+            for(int i=0; i< tag.Length; i++)
+            {
+                coordinate = field.GetLocation(i);
+                x = coordinate[0];
+                y = coordinate[1];
+
+                if (Math.Abs(x - xZero) != 1 && Math.Abs(y - yZero) != 1)
+                {
+                    field.Shift(i);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ barley-break/barley-break/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace barley_break
+{
+
+    public class Game{
+        int[,] field;
+        private int SizeField;
+
+        public Game(int[] args)
+        {
+            if (Math.Sqrt(args.Length) % 1 != 0) {
+                throw new ArgumentException("Wrong number of arguments");
+            }
+            else
+            {
+                SizeField = (int)Math.Sqrt(args.Length);
+                field = new int[SizeField, SizeField];
+
+                var rnd = new Random(1);
+
+                for (int i = 0; i < SizeField; i++)
+                    for (int j = 0; j < SizeField; j++)       
+                        field[i, j] = AssignmentWithoutRpetition(args);
+            }
+        }
+
+
+        private int AssignmentWithoutRpetition(int[] args)
+        {
+
+            var rnd = new Random();
+            var value = args[rnd.Next(0, args.Length)];
+            
+
+            for(int i=0; i< 100; i++ )
+            {
+                value = args[rnd.Next(0, args.Length)];
+                if (GetLocation(value) == null)
+                    return value;
+
+            }
+            return 0;
+        }
+
+        public int this[int x, int y]
+        {
+            get {
+                if (x >= SizeField || y >= SizeField) throw new ArgumentException("Incorrectly sets the index");
+                else return field[x,y];
+            }
+            private set {
+                if(x>=SizeField || y>=SizeField) throw new ArgumentException("Incorrectly sets the index");
+                else field[x, y] = value;
+            }
+        }
+
+
+        public int[] GetLocation(int value) {
+
+            for(int i=0; i<SizeField; i++)
+                for (int j = 0; j < SizeField; j++)
+                {
+                    if (field[i, j] == value) {
+                        int[] res = { i, j };
+                        return res;
+                    }
+                }
+            return null;
+            
+        }
+       
+        public void Shift(int value) {
+            int[] coordinate = GetLocation(value);
+            int[] coordinateZero = GetLocation(0);
+            int x = coordinate[0];
+            int y = coordinate[1];
+            int xZero = coordinateZero[0];
+            int yZero = coordinateZero[1];
+
+            if (Math.Abs(x - xZero) == 1 && Math.Abs(y - yZero) == 0
+             || Math.Abs(x - xZero) == 0 && Math.Abs(y - yZero) == 1) {
+
+                field[x, y] = 0;
+                field[xZero, yZero]=value;
+
+            }
+            else throw new ArgumentException("Zero is not on an adjacent site");
+
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
