b18135b14b650883d594145f5b90e8a12d3d7c8a
MenkinaNataliya <tasia-@mail.ru>
2016-11-17 13:41:41 +0500

    Первая часть задания

---
+++ EighthWeek/ReadCsv/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCsv
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+            foreach (var line in ReadCsv.ReadCsv1)
+            {
+              //  Console.WriteLine(line.);
+            }
+        }
+    }
+}
---
+++ EighthWeek/ReadCsv/ReadCsv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCsv
+{
+    public class ReadCsv
+    {
+        public static string[] lines = File.ReadAllLines("airquality.csv");
+
+        public static IEnumerable<string[]> ReadCsv1
+        {
+            get
+            {
+                foreach (var t in lines)
+                {
+                    var line = t.Split(',');
+                    for (var j = 0; j < line.Length; j++)
+                    {
+                        if (line[j] == "NA") line[j] = null;
+                    }
+                    yield return line;
+                }
+            }
+        }
+
+    }
+}
